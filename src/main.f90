ff8f0cf933138811b5c3ce962f010b6e76f8cee7
gcp <k-iwanaga@example.com>
2019-12-23 15:51:29 +0000

    moduleのuseの動作確認

--- src/main.f90
+++ src/main.f90
@@ -1,7 +1,16 @@
 program hello
 
-  implicit none
+  use integration
+  use calculation
+  use parameters
 
-  print *,"Hello"
+  implicit none
+  real(8) :: tmp
+
+  call calc_Sigma(0.0d0, 0.0d0, tmp)
+  print *, tmp
+
+  call calc_Delta(0.0d0, tmp)
+  print *, tmp
 
 end program hello
